10b28cbb3e167f67f62e558ae41ec05961fb5414
evshiron <evshiron@gmail.com>
2015-06-10 02:00:51 +0800

    Level XMP Burster

--- src/main/java/info/evshiron/ingresscraft/items/XMPBursterItem.java
+++ src/main/java/info/evshiron/ingresscraft/items/XMPBursterItem.java
@@ -1,14 +1,18 @@
 package info.evshiron.ingresscraft.items;
 
+import info.evshiron.ingresscraft.Constants;
 import info.evshiron.ingresscraft.IngressCraft;
 import info.evshiron.ingresscraft.entities.PortalEntity;
 import info.evshiron.ingresscraft.entities.ResonatorEntity;
 import info.evshiron.ingresscraft.utils.IngressHelper;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EntityDamageSource;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -31,6 +35,36 @@ public class XMPBursterItem extends Item {
     }
 
     @Override
+    public void addInformation(ItemStack itemStack, EntityPlayer player, List lines, boolean p_77624_4_) {
+
+        if(!itemStack.hasTagCompound()) {
+
+            return;
+
+        }
+
+        NBTTagCompound nbt = itemStack.getTagCompound();
+
+        lines.add(String.format("L%d", nbt.getInteger("level")));
+
+    }
+
+    @Override
+    public void onUpdate(ItemStack itemStack, World world, Entity entity, int p_77663_4_, boolean p_77663_5_) {
+
+        if(!itemStack.hasTagCompound()) {
+
+            NBTTagCompound nbt = new NBTTagCompound();
+
+            nbt.setInteger("level", 1);
+
+            itemStack.setTagCompound(nbt);
+
+        }
+
+    }
+
+    @Override
     public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
 
         ItemStack scanner = player.getCurrentArmor(3);
@@ -44,28 +78,38 @@ public class XMPBursterItem extends Item {
 
         }
 
-        // Should be fetched as below when leveling available.
-        float xmpBursterRange = 10;
-        float xmpBursterDamage = 500;
-        //float xmpBursterDamage = mDamage;
+        NBTTagCompound nbt = scanner.getTagCompound();
 
-        List<ResonatorEntity> resonators = IngressHelper.GetEntitiesAround(world, ResonatorEntity.class, player, xmpBursterRange);
+        if(nbt.getInteger("level") < itemStack.getTagCompound().getInteger("level")) {
+
+            String broadcast = String.format("You don't have the access to fire this XMP Burster.");
+            player.addChatMessage(new ChatComponentText(broadcast));
+
+            return itemStack;
+
+        }
+
+        // Should be fetched as below when leveling available.
+        double range = IngressHelper.GetXMPBursterRange(itemStack.getTagCompound().getInteger("level"));
+        double damage = IngressHelper.GetXMPBursterDamage(itemStack.getTagCompound().getInteger("level"));
+
+        List<ResonatorEntity> resonators = IngressHelper.GetEntitiesAround(world, ResonatorEntity.class, player, range);
 
         for(int i = 0; i < resonators.size(); i++) {
 
             ResonatorEntity resonator = resonators.get(i);
 
-            resonator.attackEntityFrom(new EntityDamageSource(IngressCraft.MODID + ":xmpBurster", player), xmpBursterDamage);
+            resonator.attackEntityFrom(new EntityDamageSource(IngressCraft.MODID + ":xmpBurster", player), (float) damage);
 
         }
 
-        List<PortalEntity> portals = IngressHelper.GetEntitiesAround(world, PortalEntity.class, player, xmpBursterRange);
+        List<PortalEntity> portals = IngressHelper.GetEntitiesAround(world, PortalEntity.class, player, range);
 
         for(int i = 0; i < portals.size(); i++) {
 
             PortalEntity portal = portals.get(i);
 
-            portal.attackEntityFrom(new EntityDamageSource(IngressCraft.MODID + ":xmpBurster", player), xmpBursterDamage);
+            portal.attackEntityFrom(new EntityDamageSource(IngressCraft.MODID + ":xmpBurster", player), (float) damage);
 
         }
 
--- src/main/java/info/evshiron/ingresscraft/utils/IngressHelper.java
+++ src/main/java/info/evshiron/ingresscraft/utils/IngressHelper.java
@@ -11,6 +11,56 @@ import java.util.List;
  */
 public class IngressHelper {
 
+    public static double GetXMPBursterRange(int level) {
+
+        switch(level) {
+            case 1:
+                return 39;
+            case 2:
+                return 44;
+            case 3:
+                return 54;
+            case 4:
+                return 69;
+            case 5:
+                return 89;
+            case 6:
+                return 109;
+            case 7:
+                return 134;
+            case 8:
+                return 164;
+            default:
+                return 0;
+        }
+
+    }
+
+    public static double GetXMPBursterDamage(int level) {
+
+        switch(level) {
+            case 1:
+                return 125;
+            case 2:
+                return 250;
+            case 3:
+                return 417;
+            case 4:
+                return 750;
+            case 5:
+                return 1000;
+            case 6:
+                return 1250;
+            case 7:
+                return 1500;
+            case 8:
+                return 2250;
+            default:
+                return 0;
+        }
+
+    }
+
     public static double GetDistanceBetween(Entity entityA, Entity entityB) {
 
         return Math.sqrt(Math.pow(entityA.posX - entityB.posX, 2) + Math.pow(entityA.posY - entityB.posY, 2) + Math.pow(entityA.posZ - entityB.posZ, 2));
