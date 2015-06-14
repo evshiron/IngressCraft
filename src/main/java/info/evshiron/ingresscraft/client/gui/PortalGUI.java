97bf901ff6ad300e8f560fd799abc4f2612b0481
evshiron <evshiron@gmail.com>
2015-06-15 03:36:02 +0800

    Alter PortalGUI

--- src/main/java/info/evshiron/ingresscraft/client/gui/PortalGUI.java
+++ src/main/java/info/evshiron/ingresscraft/client/gui/PortalGUI.java
@@ -1,14 +1,25 @@
 package info.evshiron.ingresscraft.client.gui;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import info.evshiron.ingresscraft.CommonProxy;
 import info.evshiron.ingresscraft.Constants;
 import info.evshiron.ingresscraft.IngressCraft;
 import info.evshiron.ingresscraft.entities.PortalEntity;
+import info.evshiron.ingresscraft.entities.ResonatorEntity;
 import info.evshiron.ingresscraft.messages.SyncPortalMessage;
+import info.evshiron.ingresscraft.utils.IngressHelper;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.texture.TextureUtil;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
+
+import javax.sound.sampled.Port;
+import java.util.List;
 
 /**
  * Created by evshiron on 6/6/15.
@@ -17,13 +28,16 @@ public class PortalGUI extends GuiScreen {
 
     public static final int ID = 10000;
 
-    public static final int ID_LINK_BUTTON = 0;
-    public static final int ID_DONE_BUTTON = 1;
-    public static final int ID_BACK_BUTTON = 2;
-    public static final int ID_EDIT_BUTTON = 3;
+    public static final int ID_BACK_BUTTON = 0;
+    public static final int ID_EDIT_BUTTON = 1;
+    public static final int ID_LINK_BUTTON = 2;
 
     EntityPlayer mPlayer;
     PortalEntity mPortal;
+    List<ResonatorEntity> mResonators;
+
+    boolean mIsEditing = false;
+    boolean mIsLinking = false;
 
     GuiTextField mPortalNameField;
 
@@ -36,6 +50,7 @@ public class PortalGUI extends GuiScreen {
 
         mPlayer = player;
         mPortal = portal;
+        mResonators = IngressHelper.GetEntitiesAround(portal.worldObj, ResonatorEntity.class, portal, IngressCraft.CONFIG_PORTAL_RANGE);
 
     }
 
@@ -48,7 +63,6 @@ public class PortalGUI extends GuiScreen {
 
         mBackButton = new GuiButton(ID_BACK_BUTTON, 0, 0, 40, 20, " < ");
         mEditButton = new GuiButton(ID_EDIT_BUTTON, 0, 20 + 10, 80, 20, "Edit");
-        mDoneButton = new GuiButton(ID_DONE_BUTTON, 0, 40 + 20, 80, 20, "Done");
         mLinkButton = new GuiButton(ID_LINK_BUTTON, (width - 200) / 2, height - 40, 200, 20, "LINK");
 
         buttonList.add(mBackButton);
@@ -79,31 +93,58 @@ public class PortalGUI extends GuiScreen {
 
             case ID_BACK_BUTTON:
 
-                mc.displayGuiScreen(null);
-                mc.setIngameFocus();
+                if(mIsEditing) {
+
+                    mIsLinking = false;
+
+                }
+                else if(mIsLinking) {
+
+                    mIsLinking = false;
+
+                }
+                else {
+
+                    mc.displayGuiScreen(null);
+                    mc.setIngameFocus();
+
+                }
 
                 break;
 
             case ID_EDIT_BUTTON:
 
-                mPortalNameField.setFocused(true);
+                if(!mIsEditing) {
 
-                buttonList.add(mDoneButton);
+                    mIsEditing = true;
 
-                buttonList.remove(mEditButton);
+                    mPortalNameField.setFocused(true);
 
-                break;
+                }
+                else {
 
-            case ID_DONE_BUTTON:
+                    mPortalNameField.setFocused(false);
 
-                write();
+                    write();
 
-                mc.displayGuiScreen(null);
-                mc.setIngameFocus();
+                    mIsEditing = false;
+
+                }
 
                 break;
 
             case ID_LINK_BUTTON:
+
+                if(!mIsLinking) {
+
+                    mIsLinking = true;
+
+                }
+                else {
+
+                    mIsLinking = false;
+
+                }
 
                 break;
 
@@ -116,9 +157,42 @@ public class PortalGUI extends GuiScreen {
     public void drawScreen(int p_73863_1_, int p_73863_2_, float p_73863_3_) {
         //super.drawScreen(p_73863_1_, p_73863_2_, p_73863_3_);
 
+        if(!mIsEditing) {
+
+            if(mPlayer.capabilities.isCreativeMode) {
+
+                mEditButton.displayString = "Edit";
+
+                mEditButton.visible = true;
+                mEditButton.enabled = true;
+
+            }
+
+        }
+        else {
+
+            mEditButton.displayString = "Done";
+
+            mEditButton.visible = true;
+            mEditButton.enabled = true;
+
+        }
+
         drawDefaultBackground();
 
         mPortalNameField.drawTextBox();
+
+        if(!mIsLinking) {
+
+            drawResonators();
+            drawMods();
+
+        }
+        else {
+
+            drawLinkablePortals();
+
+        }
 
         for(int i = 0; i < buttonList.size(); i++) {
 
@@ -127,6 +201,18 @@ public class PortalGUI extends GuiScreen {
             button.drawButton(mc, p_73863_1_, p_73863_2_);
 
         }
+
+    }
+
+    void drawResonators() {
+
+    }
+
+    void drawMods() {
+
+    }
+
+    void drawLinkablePortals() {
 
     }
 
